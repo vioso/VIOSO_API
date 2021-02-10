867b452df1f3694bae08bb7c1d41f02092f7f17e
sven.hahne@vioso.com <sven.hahne@vioso.com>
2021-02-10 19:23:18 -0300

    reversed wrong comment

--- VIOSOWarpBlend/DX/DX12WarpBlend.h
+++ VIOSOWarpBlend/DX/DX12WarpBlend.h
@@ -64,7 +64,7 @@ public:
 	* @param [IN,OPT]	nBlendChannels number of other channels to blend with
 	* @param [IN,OPT]	pBlendChannels a list of handles of other channels
     * @return TRUE on success, FALSE otherwise */
-	//VWB_ERROR DX9WarpBlend::setProjItem( D3DXMATRIX* pModelView, LPCSTR path, int nBlendChannels, DX9WarpBlend* pBlendChannels );
+	//VWB_ERROR DX9WarpBlend::setModel( D3DXMATRIX* pModelView, LPCSTR path, int nBlendChannels, DX9WarpBlend* pBlendChannels );
 
     virtual VWB_ERROR Render( VWB_param inputTexture, VWB_uint stateMask );  
 
